a638d0a18746d4404dc4d50c9a47a54c56e1353d
guenter <guenter.jaeger@gmx.de>
2025-07-09 16:06:14 +0200

    full functional

---
+++ src/app/table-of-contents/searchSheetQueryModal.component.ts
@@ -0,0 +1,52 @@
+import { Component, Inject, inject, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { MatDialog, MatDialogModule, MatDialogRef, MatDialogActions, MatDialogContent, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Sheet } from '../data/Sheet';
+
+@Component({
+    selector: 'app-query-modal',
+    templateUrl: './searchSheetQueryModal.component.html',
+    styleUrl: './searchSheetQueryModal.component.css',
+    imports: [ MatFormFieldModule, MatInputModule, MatDialogModule, NgFor ],
+})
+
+export class SheetQueryModalComponent implements OnInit {
+
+    selectedSheet: Sheet | null = null;
+    sheetList: Sheet[] = [];
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private dialogRef: MatDialogRef<SheetQueryModalComponent>
+    ) {
+        this.sheetList = data;
+    }
+
+    ngOnInit(): void {
+        console.log("liste: ", this.sheetList);
+    }
+
+    //readonly dialog = inject(MatDialog);
+    onSubmit(sheet: Sheet) {
+        this.dialogRef.close(sheet);
+    }
+
+    search(searchString: string) {
+        console.log("search for ", searchString);
+
+    }
+
+    selectSheet(arg: Sheet): void {
+        console.log("clicked", arg);
+        this.selectedSheet = arg;
+    }
+
+    clickedAddSheetButton() {
+        if (this.selectedSheet != null) {
+            console.log("add: ", this.selectedSheet);
+            this.dialogRef.close(this.selectedSheet);
+        }
+    }
+}
